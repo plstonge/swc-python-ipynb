26da688b72542ea1df3dace95c3c8574017f9249
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2015-11-08 21:00:58 -0500

    Improved date format

--- push_ipynb.sh
+++ push_ipynb.sh
@@ -8,7 +8,7 @@ fi
 DIFF=$(git diff $1 | head -n 10)
 
 if [ -n "$DIFF" ]; then
-  DATE=$(date "+%Y-%m-%d_%H-%M-%S")
+  DATE=$(date "+%Y-%m-%d_%H:%M:%S")
   git add $1
   git commit -m "Pushed version: $DATE"
   git pull && git push
